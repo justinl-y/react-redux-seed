f4b5b6901d012c355c51c2dcd1348d770ff65ad6
justinl-y <justin@sumofparts.ca>
2017-05-01 20:23:29 -0700

    Webpack config modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,107 @@
-const path = require('path');
 // const webpack = require('webpack');
+const path = require('path');
+const AutoprefixerPlugin = require('autoprefixer');
+
+const styleLoaders = [
+  {
+    loader: 'css-loader',
+    options: {
+      modules: true,
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: () => [
+        AutoprefixerPlugin({ browsers: 'last 2 versions' }),
+      ],
+    },
+  },
+  {
+    loader: 'sass-loader',
+  },
+];
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: path.resolve(__dirname, './public/index.html'),
-  filename: 'index.html',
+  template: path.resolve(__dirname, './src/index.html'),
+  // filename: 'index.html',
   inject: 'body',
 });
 
-module.exports = {
+const config = {
   context: path.resolve(__dirname, './src'),
-  entry: {
-    app: './index.js',
-  },
+  entry: [
+    'babel-polyfill',
+    './index.js',
+  ],
   output: {
     filename: 'app-bundle.js',
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
   },
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, './src'),
+    historyApiFallback: true,
+    stats: {
+      children: false,
+    },
   },
-  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
-        enforce: 'pre',
         test: /\.js$/,
-        exclude: '/node_modules/',
-        loader: 'eslint-loader',
-        options: {
-          fix: true,
-          failOnError: true,
-        },
+        enforce: 'pre',
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'eslint-loader',
+          options: {
+            fix: true,
+            failOnError: false,
+          },
+        }],
       },
       {
         test: /\.js$/,
         exclude: [/node_modules/],
-        loader: 'babel-loader',
-        options: { presets: ['es2015', 'es2016', 'es2017'] },
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'es2015',
+              'es2016',
+              'es2017',
+            ],
+          },
+        }],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        exclude: [/node_modules/],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: styleLoaders,
+        }),
       },
     ],
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new ExtractTextPlugin('styles.css'),
+  ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = ''; // No sourcemap for production
+  config.output.publicPath = './';
+
+  // Add more configuration for production here like
+  // SASS & CSS loaders
+  // Offline plugin
+  // Etc,
+}
+
+module.exports = config;
